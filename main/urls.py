6cb903f765fcf7d0a23c6fb461fba3da19ea1b16
Maximiliano Abraham <maxabrahamproyectos@gmail.com>
2024-05-31 22:22:37 -0400

    se agrega app web para trabajo de hito 2

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    
     
 ]
 
---
+++ web/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from web.views import index, about, welcome
+
+
+urlpattern =[
+    path ('', index),
+    path('/about', about),
+    path ('/welcome', welcome)
+    
+]
