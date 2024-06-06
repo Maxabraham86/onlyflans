8f0ff41a663105ec56b66b6a279e1b0ab5364df1
Maximiliano Abraham <maxabrahamproyectos@gmail.com>
2024-06-06 19:18:14 -0400

    se agrega CSS como static y boton en exite.html

--- web/urls.py
+++ web/urls.py
@@ -5,7 +5,7 @@ from web.views import index, about, welcome, detalleFlan, contact_form
 urlpatterns =[
     path ('', index),
     path('about/', about),
-    path ('welcome/', welcome),
-    path('flan/<id>/', detalleFlan),
-    path('contact_form', contact_form )
+    path ('welcome/', welcome, name="welcome"),
+    path('flan/<id>/', detalleFlan, name="flan id"),
+    path('contact_form', contact_form, name="contact form" )
 ]
