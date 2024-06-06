284700bf09d516066794b2c12b05a7dc261db0e8
Maximiliano Abraham <maxabrahamproyectos@gmail.com>
2024-06-05 23:04:33 -0400

    se completa formulario con redireccion y verificadores

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from web.views import index, about, welcome, detalleFlan
+from web.views import index, about, welcome, detalleFlan, contact_form
 
 
 urlpatterns =[
     path ('', index),
     path('about/', about),
     path ('welcome/', welcome),
-    path('flan/<id>/', detalleFlan)
+    path('flan/<id>/', detalleFlan),
+    path('contact_form', contact_form )
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from web.postres import postres
 
 
@@ -29,3 +30,39 @@ def about(req):
 
 def welcome(req):
     return render(req, 'welcome.html')
+
+def contact_form(req):
+    errores=[]
+    
+    customer_name = req.POST["customer_name"]
+    customer_email = req.POST["customer_email"]
+    message = req.POST["message"]
+    
+    if len(customer_name  )> 64:
+        errores.append('largo mayor a 64 caracteres')
+        
+    
+    if not '@' in customer_email:
+        errores.append('Falta el arroba')
+
+        context ={'errores' : errores}
+    if len(errores) > 0:
+        return render(req, 'welcome.html',context)
+    else:
+        return render(req, 'exito.html')
+    
+    
+        #return redirect ("/nombre_pagina")
+    
+    
+    
+    
+    print(customer_email)
+    #return HttpResponse ("Tamos")
+    
+
+    #ahora tengo que validar que customer_email tenga al menos una @
+    #y customer_name sea de largo maximo 64 
+    #
+    #si len errores == 0 : redirijo a pagina de exito
+    # si len errores > 0 : vuelvo a vargar 'welcome.html', mostrando los errores
